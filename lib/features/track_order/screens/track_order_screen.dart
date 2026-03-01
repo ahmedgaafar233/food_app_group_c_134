3dfac3719467fb43a9467decf81fbba83949ffec
Ahmed Iraqey <ahmediraqey1@gmail.com>
2026-03-01 21:42:36 +0200

    done

---
+++ lib/features/track_order/screens/track_order_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TrackOrderScreen extends StatefulWidget {
+  const TrackOrderScreen({super.key});
+
+  @override
+  State<TrackOrderScreen> createState() => _TrackOrderScreenState();
+}
+
+class _TrackOrderScreenState extends State<TrackOrderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My Orders')),
+      body: Center(),
+    );
+  }
+}
